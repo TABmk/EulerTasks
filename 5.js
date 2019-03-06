ad24651624d0033517c8b3fa9e795a2b6d2b59c3
TAB_mk <tabmk999@gmail.com>
2019-03-06 15:23:49 +0300

    review

--- 5.js
+++ 5.js
@@ -4,6 +4,6 @@ let num = 20;
 let _numbers = [...Array(21).keys()].slice(1);
 
 while (true) {
-	if (_numbers.every(d => !(num % d))) break;
+	if (_numbers.every(d => num % d === 0)) break;
 	else num++;
 }
