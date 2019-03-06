22c8c6ef4bfbfaa8b120591f83775285a9a1817b
TAB_mk <tabmk999@gmail.com>
2019-03-06 17:12:18 +0300

    +10ms

--- 7.js
+++ 7.js
@@ -1,5 +1,5 @@
 let isPrime = num => {
-    for (let i = 2, s = Math.sqrt(num); i <= s; i++) if (num % i === 0) return false;
+    for (let i = 3, s = Math.sqrt(num); i <= s; i += 2) if (num % i === 0) return false;
     return num > 1;
 }
 
