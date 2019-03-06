bcb2b0f4b846f31cf667ff97afbacb82e8b7d41e
TAB_mk <tabmk999@gmail.com>
2019-03-06 04:01:56 +0300

    fix + faster palindrome check

--- 4.js
+++ 4.js
@@ -1,12 +1,24 @@
-let palindrome = 0;
+let p = 0;
+// find in X*X
+let toFind = 999;
 
-main : for (let f = 999; f >= 0; f--) {
-	for (let s = 999; s >= 0; s--) {
-		num = f*s;
-		s_num = `${num}`.split('');
-		if (s_num.join('') === s_num.reverse().join('')) {
-			palindrome = num;
-			break main;
-		}
-	}
+let reversenum = num => {
+    num = num + '';
+    cursor = 0
+    while (true) {
+        if (cursor > Math.ceil(num.length) / 2) break;
+        else if (num[cursor] !== num[num.length - cursor - 1]) return false;
+        else cursor++
+    }
+    return true;
+}
+
+for (let f = toFind; f >= 100; f--) {
+    if (f * toFind < p) break;
+    for (let s = f; s >= 100; s--) {
+        let num = f * s;
+        if (num > p) {
+            if (reversenum(num)) p = num;
+        } else break;
+    }
 }
